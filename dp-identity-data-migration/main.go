167df0a172a2615f1a40e78356fa85df55372587
AWitcherONS <ann.witcher@ons.gov.uk>
2022-05-23 16:37:05 +0100

    refactorisation second attempt

--- dp-identity-data-migration/main.go
+++ dp-identity-data-migration/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/ONSdigital/dp-data-tools/groupsdataextraction"
+	"github.com/ONSdigital/dp-data-tools@v0.0.0-20220426125527-a81d87f6fdb3/dp-identity-data-migration/groupsdataextraction"
 )
 
 func main() {
 	fmt.Println("Hello World")
+
 	groupsdataextraction.ExtractGroupData()
 }
